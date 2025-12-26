c154c0574111de874a927c11ad8fc0ac9a35c353
Fortune <osaserefortune@gmail.com>
2025-12-26 21:45:49 +0100

    Fix: Use Heroku PORT env variable

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const { v4: uuidv4 } = require('uuid');
 const { runPipeline, getJobStatus } = require('../scripts/orchestrator');
 
 const app = express();
-const PORT = process.env.API_PORT || 3001;
+const PORT = process.env.PORT || process.env.API_PORT || 3001;
 
 // Middleware
 app.use(cors());
